050e29161fdba4caf63e6cb46d187dde0d9a3f4c
Thomas Lobjois <Thomaslobjois@ymail.com>
2024-07-19 17:20:34 +0200

    nouvelle branch changement de place dans function.js

---
+++ FrontEnd/function.js
@@ -0,0 +1,18 @@
+function updateAuthButton(authBtn) {
+    const token = localStorage.getItem('token');
+    if (token) {
+        authBtn.textContent = 'Logout';
+        authBtn.href = '#'; // Pas besoin de lien pour déconnexion
+        authBtn.addEventListener('click', () => {
+            // Déconnexion
+            localStorage.removeItem('token');
+            authBtn.textContent = 'Login';
+            authBtn.href = 'login.html'; // Redirige vers la page de connexion
+            alert('Vous avez été déconnecté.');
+            window.location.href = 'login.html'; // Redirige vers la page de connexion
+        });
+    } else {
+        authBtn.textContent = 'Login';
+        authBtn.href = 'login.html'; // Redirige vers la page de connexion
+    }
+}
--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,25 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
     const authBtn = document.getElementById('auth-btn');
 
-    function updateAuthButton() {
-        const token = localStorage.getItem('token');
-        if (token) {
-            authBtn.textContent = 'Logout';
-            authBtn.href = '#'; // Pas besoin de lien pour déconnexion
-            authBtn.addEventListener('click', () => {
-                // Déconnexion
-                localStorage.removeItem('token');
-                authBtn.textContent = 'Login';
-                authBtn.href = 'login.html'; // Redirige vers la page de connexion
-                alert('Vous avez été déconnecté.');
-                window.location.href = 'login.html'; // Redirige vers la page de connexion
-            });
-        } else {
-            authBtn.textContent = 'Login';
-            authBtn.href = 'login.html'; // Redirige vers la page de connexion
-        }
-    }
+   
 
     // Met à jour le bouton au chargement de la page
-    updateAuthButton();
+    updateAuthButton(authBtn);
+    
 });
